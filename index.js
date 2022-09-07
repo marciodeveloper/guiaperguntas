032faa5f044b06f3ee92d5efa508e2b96d1df086
Jose Marcio <marciodeveloper@outlook.com>
2022-09-07 13:56:48 -0300

    feat: exibindo variáveis no html

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const app = express();
 //Estou pedindo para o express usar o EJS como view engine
 app.set("view engine", "ejs");	
 
-app.get("/", (req, res) => {
-    res.render("index");
+app.get("/:nome/:lang", (req, res) => {
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    res.render("index", {
+       nome: nome,
+       lang: lang,
+       empresa: "DevPleno",
+       inscritos: 8000
+    });
 });
 
 app.listen(8080, () => {console.log("App rodando na porta 8080");});
